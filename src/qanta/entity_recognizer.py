cbfe9b82e6cfc956365b4d58142c59f2de384381
Craig Thorburn <craigtho@umd.edu>
2018-11-12 19:40:22 -0500

    Finished entity class and unit tests

--- src/qanta/entity_recognizer.py
+++ src/qanta/entity_recognizer.py
@@ -1,36 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Craig Thorburn
+"""
 from nltk.tokenize import word_tokenize
+from nltk import FreqDist
 import re
 
-class EntityRecognizer:
+class ThisEntity:
     
-    def __init__(self):
-        self._entities = ['UNK']
+    def __init__(self, threshold = 2, exceptions = ['is']):
+        # Threshold: Number of mentions required to be entity
+        # Exceptions: Words which should never be entities
+        self._entities = FreqDist()
+        self._exceptions = exceptions
+        self._threshold = threshold
         self._finalized = False
         self._this_regex = re.compile('(^|\s)(t|T)his \w*')
         
     def get_entity(self, sentence):
+        # Returns entity of sentence
         entity = word_tokenize(self._this_regex.search(sentence).group())[1]
         return entity
         
-    def add_sentence(self, sentence, return_entity=False):
+    def add_sentence(self, sentence):
+        # Adds entity of sentence to vocab
         assert not self._finalized, 'Vocab is already finalized'
-        
         entity = self.get_entity(sentence)
-        if entity not in self._entities:
-            self._entities.append(entity)
-        if return_entity:
-            return entity
-
+        self._entities[entity]+=1
+        
     def finalize_vocab(self):
+        # Finalizes vocab for testing
+        # Returns number of entitites (including UNK)
+        self._final_entities = [ee for ee in self._entities if self._entities[ee] >= self._threshold]
+        for xx in self._exceptions:
+            if xx in self._final_entities:
+                self._final_entities.remove(xx)   
+  
+        self._final_entities = ['UNK'] + self._final_entities               
         self._finalized = True
+        return len(self._final_entities)
     
-    def get_entity_vector(self, sentence):
+    def get_entity_index(self, sentence):
+        # Returns index of entity (0 if UNK)
         assert self._finalized, 'Vocab not finalized'
         sentence_entity = self.get_entity(sentence)
-        if sentence_entity not in self._entities:
-            return 'UNK'
+        if sentence_entity not in self._final_entities:
+            return 0
         else:
-            return sentence_entity
+            return self._final_entities.index(sentence_entity)
         
     def get_all_entities(self):
-        return self._entities
+        # Returns list of all entities
+        assert self._finalized, 'Vocab not finalized'
+        return self._final_entities
