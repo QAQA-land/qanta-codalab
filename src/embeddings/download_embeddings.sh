0d980017e9143994cd143169207d5c1894f0d334
Jason Fan <jason.fan.74@gmail.com>
2018-11-26 17:22:36 -0500

    wiki2vec embeddings and gitignore

---
+++ src/embeddings/download_embeddings.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash
+
+wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_100d.txt.bz2 && \
+wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_300d.txt.bz2
